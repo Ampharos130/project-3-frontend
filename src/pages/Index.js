bbf438b1168c332eeda0190670728608e29a279a
Timmy Do <timmy.do130@gmail.com>
2022-02-03 07:42:00 -0700

    Index css

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,43 +1,18 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import './Index.scss'
 
 function Index(props) {
-  // state to hold formData
-  const [newForm, setNewForm] = useState({
-    title: "",
-    img: "",
-    author: "",
-    descript: "",
-    price: "",
-    qty: ""
-  });
 
-  // handleChange function for form
-  const handleChange = (event) => {
-    setNewForm({ ...newForm, [event.target.name]: event.target.value });
-  };
-
-  // handle submit function for form
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    props.createBook(newForm);
-    setNewForm({
-      title: "",
-      img: "",
-      author: "",
-      descript: "",
-      price: "",
-      qty: ""
-    });
-  };
   // loaded function
   const loaded = () => {
     return props.book.map((books) => (
+      
       <div key={books._id} className="books">
-        <Link to={`/books/${books._id}`}>
           <h1>{books.title}</h1>
+        <Link to={`/books/${books._id}`}>
+        <img src={books.img} alt={books.title} className="indexImg"/>
         </Link>
-        <img src={books.img} alt={books.title} />
         <h3>{books.author}</h3>
       </div>
     ));
@@ -49,52 +24,10 @@ function Index(props) {
 
   return (
     <section>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newForm.title}
-          name="title"
-          placeholder="title"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newForm.img}
-          name="img"
-          placeholder="image URL"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newForm.author}
-          name="author"
-          placeholder="author"
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          value={newForm.descript}
-          name="descript"
-          placeholder="description"
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          value={newForm.price}
-          name="price"
-          placeholder="price"
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          value={newForm.qty}
-          name="qty"
-          placeholder="quantity"
-          onChange={handleChange}
-        />
-        <input type="submit" value="Create Book" />
-      </form>
+      <div className="indexDiv">
       {props.book ? loaded() : loading()}
+      </div>
+
     </section>
   );
 }
